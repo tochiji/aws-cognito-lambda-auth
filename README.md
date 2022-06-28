b05cbf852ff549e506179bfcce182e721f1c4ca3
tochiji <7910558+tochiji@users.noreply.github.com>
2022-06-29 02:51:18 +0900

    create-user関数を更新

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # aws-cognito-lambda-auth
 
 Cognitoのユーザープールを使った認証について深く学ぶためのリポジトリ
+
+
+## CFnのStack削除
+すべての課金やLambda、DynamoDBの利用を防御する
+
+```bash
+$ aws cloudformation delete-stack --stack-name aws-cognito-lambda-auth-app
+```
--- src/handlers/create-user.js
+++ src/handlers/create-user.js
@@ -1,42 +1,82 @@
 const AWS = require("aws-sdk");
 const cognito = new AWS.CognitoIdentityServiceProvider();
 
-const userPoolId = process.env.USER_POOL_ID;
+const clientId = process.env.CLIENT_ID;
 
 exports.createUserHandler = async (event) => {
+  console.log("EVENT\n", event);
+
   if (event.httpMethod !== "POST") {
+    console.log("ERROR\n", "httpMethod is not POST");
     throw new Error(
       `postMethod only accepts POST method, you tried: ${event.httpMethod} method.`
     );
   }
 
-  // Get id and name from the body of the request
+  console.log("clientId\n", clientId);
+
   const body = JSON.parse(event.body);
   const email = body.email;
   const password = body.password;
 
-  // SignUpのパラメーター
   const params = {
-    UserPoolId: userPoolId,
-    Username: email,
-    Password: password,
-    UserAttributes: [
-      { Name: "email", Value: email }, // 必要に応じて増減する。
-    ],
+    ClientId: clientId, // アプリケーションクライアントのID
+    Username: email, // template.yamlより、「UsernameAttributes」は「email」を指定。
+    Password: password, // template.yamlより、PasswordPolicyにてパスワードの複雑性を指定。
+    UserAttributes: [{ Name: "email", Value: email }],
   };
 
-  // SignUp実行
   const result = await cognito
     .signUp(params)
     .promise()
     .catch((error) => {
-      // 必要に応じて例外処理を追加する。
-      // 例えば、IDが重複したときの例外は→「error.code == 'UsernameExistsException'」
-      throw error;
+      //
+      // errorの例
+      //
+      // error = {
+      //   "code": "InvalidPasswordException",
+      //   "message": "Password did not conform with policy: Password not long enough",
+      //   "requestId": "07c7eafe-bff7-4777-bc51-a8d4acc8c0dc",
+      //   "retryDelay": 38.45781588499835,
+      //   "retryable": false,
+      //   "statusCode": 400,
+      //   "time": "2022-06-27T17:13:05.930Z"
+      // }
+      //
+      console.log("ERROR\n", error);
+
+      return {
+        error: true,
+        code: error.code,
+        message: error.message,
+      };
     });
 
-  // HTTPレスポンス 必要に応じて編集する。
+  //
+  // 成功時のbodyの中身例
+  //
+  // {
+  //   "CodeDeliveryDetails": {
+  //       "AttributeName": "email",
+  //       "DeliveryMedium": "EMAIL",
+  //       "Destination": "p***@g***"
+  //   },
+  //   "UserConfirmed": false,
+  //   "UserSub": "b2377463-32a0-408b-bcff-9fea229d5990"
+  // }
+  //
+  //
+  // error時のbodyの中身例
+  //
+  // {
+  //   "error": "true",
+  //   "code": "UsernameExistsException",
+  //   "message": "An account with the given email already exists."
+  // }
+  //
+
   return {
     statusCode: 200,
+    body: JSON.stringify(result),
   };
 };
