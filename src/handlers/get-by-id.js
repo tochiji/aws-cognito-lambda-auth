ebe3645625e8983bf289e4af2cba06a1b1dba322
tochiji <7910558+tochiji@users.noreply.github.com>
2022-07-07 23:21:22 +0900

    CORSの設定を追加

--- src/handlers/get-by-id.js
+++ src/handlers/get-by-id.js
@@ -4,37 +4,48 @@
 const tableName = process.env.SAMPLE_TABLE;
 
 // Create a DocumentClient that represents the query to add an item
-const dynamodb = require('aws-sdk/clients/dynamodb');
+const dynamodb = require("aws-sdk/clients/dynamodb");
 const docClient = new dynamodb.DocumentClient();
 
 /**
  * A simple example includes a HTTP get method to get one item by id from a DynamoDB table.
  */
 exports.getByIdHandler = async (event) => {
-  if (event.httpMethod !== 'GET') {
-    throw new Error(`getMethod only accept GET method, you tried: ${event.httpMethod}`);
+  if (event.httpMethod !== "GET") {
+    throw new Error(
+      `getMethod only accept GET method, you tried: ${event.httpMethod}`
+    );
   }
   // All log statements are written to CloudWatch
-  console.info('received:', event);
- 
+  console.info("received:", event);
+
   // Get id from pathParameters from APIGateway because of `/{id}` at template.yaml
   const id = event.pathParameters.id;
- 
+
   // Get the item from the table
   // https://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/DynamoDB/DocumentClient.html#get-property
   var params = {
-    TableName : tableName,
+    TableName: tableName,
     Key: { id: id },
   };
   const data = await docClient.get(params).promise();
   const item = data.Item;
- 
+
   const response = {
     statusCode: 200,
-    body: JSON.stringify(item)
+    headers: {
+      "Content-Type": "application/json; charset=utf-8",
+      "Access-Control-Allow-Headers":
+        "Origin, Authorization, Accept, X-Requested-With, Content-Type, x-amz-date, X-Amz-Security-Token",
+      "Access-Control-Allow-Origin": "http://localhost:3000",
+      "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
+    },
+    body: JSON.stringify(item),
   };
- 
+
   // All log statements are written to CloudWatch
-  console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
+  console.info(
+    `response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`
+  );
   return response;
-}
+};
